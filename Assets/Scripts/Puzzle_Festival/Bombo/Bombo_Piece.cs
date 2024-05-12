1ef81d0d2c95fe73b5527136d73419f3769b3cab
Jonah Galahad <108025488+JonahGalahad@users.noreply.github.com>
2024-05-11 23:41:17 -0300

    BomboPuzzle fixed

--- Assets/Scripts/Puzzle_Festival/Bombo/Bombo_Piece.cs
+++ Assets/Scripts/Puzzle_Festival/Bombo/Bombo_Piece.cs
@@ -9,22 +9,15 @@ public class Bombo_Piece : MonoBehaviour
     [SerializeField] private Bombo_Controller piece;
     //private float numPiece;
     private bool isTaken;
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerStay(Collider other) {
         if (other.tag == "Player") {
-            isTaken = true;
-            if (Input.GetKeyDown(button) && isTaken) {
+            if (Input.GetKeyDown(button)) {
                 piece.NumPiece++;
                 puzzle.gameObject.SetActive(false);
                 this.gameObject.SetActive(false);
                 //numPiece++;
                 //isTaken=true;
             }
-        }
-    }
-    private void OnTriggerExit(Collider other) {
-        if (other.tag == "Player")
-        {
-            isTaken=false;
         }
     }
 }
