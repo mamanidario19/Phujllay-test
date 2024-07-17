3df3199760bfcf24399036a90745b47b94945f60
Dario Santiago Mamani <mamanidario.19@gmail.com>
2024-07-17 12:42:24 -0300

    fix

--- Assets/Scripts/Inventory_System/Slot.cs
+++ Assets/Scripts/Inventory_System/Slot.cs
@@ -15,12 +15,14 @@ public class Slot : MonoBehaviour
     public bool empty;
     public Sprite icon;
 
-    public Transform slotIconGameObject;
+    public Transform slotIconGameObject; //panel hijo
+
     public Button slotButton;
 
     private void Start()
     {
         slotIconGameObject = transform.GetChild(0);
+
         slotButton = GetComponent<Button>();
         slotButton.onClick.AddListener(OnSlotClicked);
     }
