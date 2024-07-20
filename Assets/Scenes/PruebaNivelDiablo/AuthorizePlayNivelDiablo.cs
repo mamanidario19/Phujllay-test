0e033505cbfbb8b098e82b7e3be7c8bbe9edcab3
SrNaile <elianfacundofloresdecampoverde@hotmail.com>
2024-07-20 19:10:19 -0300

    fix_puzzles_and_transition_camera_level_diablo

---
+++ Assets/Scenes/PruebaNivelDiablo/AuthorizePlayNivelDiablo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuthorizePlayNivelDiablo : MonoBehaviour
+{
+    // Variable pública para almacenar el estado de activación
+    public bool thisObjectActive;
+
+    private void OnEnable()
+    {
+        // Se llama cuando el objeto se activa
+        thisObjectActive = true;
+    }
+
+    private void OnDisable()
+    {
+        // Se llama cuando el objeto se desactiva
+        thisObjectActive = false;
+    }
+}
